367a279b8babc79b8c2b39eb8b04f1d44f253fe1
Hitman1245 <rohit.kamble@blazeclan.com>
2022-03-08 17:00:03 +0530

    third code change

---
+++ credentials.tf
@@ -0,0 +1,12 @@
+variable "username" {
+  default = "admin"
+}
+
+variable "password" {
+  default = "pass2021"
+}
+
+
+variable "instance_class" {
+  default = "db.t3.micro"
+}
---
+++ rds.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
+resource "aws_db_instance" "my-db" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = var.instance_class
+  port                 = "3306"
+  db_name              = "mydb"
+  username             = var.username
+  password             = var.password
+  parameter_group_name = "default.mysql5.7"
+  availability_zone    = "ap-south-1a"
+  publicly_accessible  = true
+  skip_final_snapshot  = true
+
+
+  tags = {
+    name = "mysql rds instance"
+  }
+}
